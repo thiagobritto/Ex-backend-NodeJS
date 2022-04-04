991d5a0b1831ac92057af46bd4ce6782aa1832b8
thiagobritto <thiagobritto.developer@gmail.com>
2022-04-04 10:26:25 -0300

    statr backend

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+axios.get('/sanctum/csrf-cookie').then(response => {
+    console.log('csrf-cookie: ', response);
+});
+
+axios.get('/users').then(response => {
+    console.log(response);
+});
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use Dflydev\DotAccessData\Data;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +19,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//::middleware('auth:sanctum')->
+
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/{id}', [UserController::class, 'show']);
+Route::get('/user/search/{name}', [UserController::class, 'search ']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::post('/user', [UserController::class, 'store']);
+    Route::put('/user/{id}', [UserController::class, 'update']);
+    Route::delete('/user/{id}', [UserController::class, 'destroy']);
 });
